987f94e010bf88c342236ef5d86b5ee8c3d0f9ab
Ibrahim <ibrahim.bedoui@ynov.com>
2025-03-05 12:25:41 +0100

    Module security_groups ok

--- infrastructure/aws/network/security_groups/outputs.tf
+++ infrastructure/aws/network/security_groups/outputs.tf
@@ -1,19 +1,4 @@
-output "bastion_instance_sg_id" {
-  description = "ID of the Bastion Security Group"
-  value       = aws_security_group.bastion_instance.id
-}
-
-output "docker_instance_sg_id" {
-  description = "ID of the Docker Security Group"
-  value       = aws_security_group.docker_instance.id
-}
-
-output "sg_bastion_lb_id" {
-  description = "ID of the Bastion Load Balancer Security Group"
-  value       = aws_security_group.lb_bastion.id
-}
-
-output "sg_docker_lb_id" {
-  description = "ID of the Docker Load Balancer Security Group"
-  value       = aws_security_group.lb_docker.id
+output "instance_sg_id" {
+  description = "ID of the Security Group"
+  value       = aws_security_group.instance_sg.id
 }
