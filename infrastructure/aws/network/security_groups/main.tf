217201d61e73494780f5c38f447f06420a2c3303
Ibrahim <ibrahim.bedoui@ynov.com>
2025-03-05 12:27:58 +0100

    Module security_groups modification rules

--- infrastructure/aws/network/security_groups/main.tf
+++ infrastructure/aws/network/security_groups/main.tf
@@ -39,14 +39,14 @@ resource "aws_security_group" "instance_sg" {
     cidr_blocks = ["10.0.0.0/16"]
   }
 
-  # # Autoriser tout le trafic sortant
-  # egress {
-  #   description = "Allow all outbound traffic"
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
+   # Autoriser tout le trafic sortant
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name  = "${var.project_name}-${var.vpc_name}-SG-EC2"
