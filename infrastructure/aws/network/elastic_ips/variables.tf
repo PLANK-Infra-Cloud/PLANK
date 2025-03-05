9c619cb3b6c5f07e6faebb900376c6f749921d79
Ibrahim <ibrahim.bedoui@ynov.com>
2025-03-05 14:02:04 +0100

    Module elastic ips ok

--- infrastructure/aws/network/elastic_ips/variables.tf
+++ infrastructure/aws/network/elastic_ips/variables.tf
@@ -1,14 +1,15 @@
-# variable "eip_count" {
-#   description = "Nombre d'Elastic IPs à créer"
-#   type        = number
-# }
+variable "eip_count" {
+  description = "Nombre d'Elastic IPs à créer"
+  type        = number
+  default     = 3
+}
 
-# variable "project_name" {
-#   description = "Nom du projet"
-#   type        = string
-# }
+variable "project_name" {
+  description = "Nom du projet"
+  type        = string
+}
 
-# variable "vpc_name" {
-#   description = "Nom du VPC"
-#   type        = string
-# }
+variable "vpc_name" {
+  description = "Nom du VPC"
+  type        = string
+}
