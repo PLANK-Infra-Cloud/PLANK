f06195416b15dd36c8cef0fe634d78c1a02fdca6
Ibrahim <ibrahim.bedoui@ynov.com>
2025-04-16 16:49:46 +0200

    ajout secret key content

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -84,8 +84,13 @@ variable "key_name" {
   default     = "PLANK-key"
 }
 
-variable "ssh_private_key_path" {
-  description = "Chemin vers la clé privée SSH"
+# variable "ssh_private_key_path" {
+#   description = "Chemin vers la clé privée SSH"
+#   type        = string
+#   default     = "~/.ssh/PLANK-key.pem"
+# }
+
+variable "ssh_private_key_content" {
+  description = "Contenu de la clé privée SSH pour la connexion aux instances"
   type        = string
-  default     = "~/.ssh/PLANK-key.pem"
 }
